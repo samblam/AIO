2fab750571cc1ed920ea7a9982c3596c0b62ad29
Chris Connors <connorschrisc@gmail.com>
2018-03-09 14:07:46 -0400

    Loading forms on the case info page

---
+++ JS/formLoader.js
@@ -0,0 +1,19 @@
+$('.active[data-toggle="tab"]').ready(function(e) {
+    $("#forma").load("forma.html");
+});
+
+$(document).ready( function() {
+    $("#forma").load("forma.html");
+});
+
+$(document).ready( function() {
+    $("#formb").load("formb.html");
+});
+
+$(document).ready( function() {
+    $("#formc").load("formc.html");
+});
+
+$(document).ready( function() {
+    $("#formd").load("formd.html");
+});
