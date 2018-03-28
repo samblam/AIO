3bd9432467bf6b039095e4c9af54982fd6cdb0e3
jmorash <js725470@dal.ca>
2018-03-28 20:25:18 -0300

    Changed the javascript adminactivecasesdelete function to redirect with necessary query string to perform delete operation. CLoser to finishing submit function for form A

--- includes/formProcess.php
+++ includes/formProcess.php
@@ -1,20 +1,20 @@
 <?php
 
+include 'db.php';
 //This makes it so no one can submit forms at the moment
-if(basename($_SERVER['PHP_SELF'])=="formProcess.php")
-header("location: ../index.php");
+//if(basename($_SERVER['PHP_SELF'])=="formProcess.php")
+//header("location: ../index.php");
 
-//this needs the prepare statements for inserting the new case and getting
-//the case id of the newly inserted case in order to upload the files.
 //Also need to decide on max upload size
 if(isset($_POST['SubmitFormA'])){
+
+  //This section gets the file ready to upload but doesnt upload it yet because we don't yet know where to put it
   $target_dir= "../evidence/";
-  $target_file= $target_dir.basename($_FILES["file"]["name"]);
+  $target_file= $target_dir.basename($_FILES["fileInput"]["name"]);
   $uploadAllowed = false;
   if($target_file != $target_dir){
     $finfo= finfo_open(FILEINFO_MIME_TYPE);
     $mime = finfo_file($finfo, $_FILES["fileInput"]["tmp_name"]);
-
     switch ($mime) {
       case '':// Allowed mimes
       case '':// Allowed mimes
@@ -32,6 +32,7 @@ if(isset($_POST['SubmitFormA'])){
     $evidence = htmlspecialchars(trim(stripslashes($target_file)));
   }
 
+  //Grabs all form data and sanatize it
   $prof = htmlspecialchars(trim(stripslashes($_POST['ProfessorName'])));
   $email = htmlspecialchars(trim(stripslashes($_POST['email'])));
   $phone = htmlspecialchars(trim(stripslashes($_POST['phoneNum'])));
@@ -42,45 +43,37 @@ if(isset($_POST['SubmitFormA'])){
   $boos = htmlspecialchars(trim(stripslashes($_POST['boos'])));
   $date = htmlspecialchars(trim(stripslashes($_POST['date'])));
   $comments = htmlspecialchars(trim(stripslashes($_POST['additionalComments'])));
-/*
-  $numStudents = sizeof($students);
-  $numBoos = sizeof($boos);
-  $studQuestionMarks = ", ?";
-  //Need some mechanism to get multiple students
-  if($numStudents = $numBoos){ //grab the hidden value from the form that tells you the number of student columns created
-    for(int i = 0; i < $numStudents; i++){
-      $studQuestionMarks .= ", ?";
-    }
-  }
-  foreach ($students as $student) {
-
-  }
-*/
-  $sname = htmlspecialchars(trim(stripslashes($_POST[''])));
-  $banner = htmlspecialchars(trim(stripslashes($_POST[''])));
-
   $offenseDate = htmlspecialchars(trim(stripslashes($_POST['date'])));
-  //get file upload working
   $description = htmlspecialchars(trim(stripslashes($_POST['additionalComments'])));
 
-  //prepare the query to authenticate csid's
-  //Need to perform another query to insert evidence directory name into table before I can submit the evidence somewhere
+  //Create new case entry
   $statement = $conn->prepare("INSERT INTO case (prof_id, class_name_code, date_aware, description,"/* evidence_fileDir*/.") VALUES (?, ?, ?, ?, ?)");
   $statement->bind_param("sssss",$_SESSION['userId'], $cname, $date, $comments); //bind initial values to the prepared statements
   if (!$statement->execute()) {
      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
   }
 
-  $evidenceFolderName = $conn->insert_id;
-  $updateEvidence = $conn->prepare("UPDATE case SET evidence_fileDir = ? WHERE case_id = ".$evidenceFolderName);
-  $updateEvidence->bind_param("s", $evidenceFolderName); //bind evidence folder name to the prepared statements
+  //Grabs case_id of the just inserted case and uses it to create the evidence folder location in the database
+  $caseId = $conn->insert_id;
+  $updateEvidence = $conn->prepare("UPDATE case SET evidence_fileDir = ? WHERE case_id = ".$caseId);
+  $updateEvidence->bind_param("s", $caseId); //bind evidence folder name to the prepared statements
   if (!$updateEvidence->execute()) {
      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
   }
-  
 
-  $target_file = $target_dir.$evidenceFolderName.$_FILES["file"]["name"];
+  //Insert students into student table
+  for($i = 0; $i < sizeof($students); $i++) {
+    if($students[$i] != NULL && $boos[$i] != NULL){
+      $statement = $conn->prepare("INSERT INTO student (csid, case_id, fname) VALUES (?, ?, ?)");
+      $statement->bind_param("ssss",$boos[$i], $caseId, $student[$i]); //bind initial values to the prepared statements
+      if (!$statement->execute()) {
+         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      }
+    }
+  }
 
+  //Gets the necessary directory location and then uploads the evidence to it
+  $target_file = $target_dir.$caseId."/".$_FILES["file"]["name"];
   if ($uploadAllowed) {
     if (move_uploaded_file($_FILES["fileInput"]["tmp_name"], $target_file)) {
       echo"File uploaded successfully.";
