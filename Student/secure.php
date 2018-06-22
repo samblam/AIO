6cb3b2073ea0dc1c393850adb9ddda7888186080
Bjorn <germandudestudios@gmail.com>
2018-06-22 12:03:31 -0300

    Init Commit
    Scripts prepared. Not yet included in files.

---
+++ Student/secure.php
@@ -0,0 +1,11 @@
+require_once '../includes/session.php';
+
+// checks if a user is logged in
+if( !$_SESSION['loggedIn'] ) {
+  header( 'HTTP/1.0 403 Forbidden' );
+  exit( 'You are not logged in.' );
+} // if a user is logged in, checks if legal user
+elseif( !in_array("student", $_SESSION['access_roles']) ) {
+  header( 'HTTP/1.0 403 Forbidden' );
+  exit( 'Insufficient access level' );
+}
