ee5118018471c894c4cfd39b26fdce5dddb5b749
blackmamba011 <yadiogbudibe@gmail.com>
2018-07-04 12:19:57 -0300

    Cleaned up code in accordance with ryan stevens recommendations

--- includes/formProcess.php
+++ includes/formProcess.php
@@ -230,8 +230,6 @@ if(isset($_POST['SaveFormA']) || isset($_POST['SubmitFormA'])){
 //The case where this Form A set aio_id 
   if(isset($_POST['AcceptFormA'])){
     
-    //header('location: ../AIO/ActiveCases.php');
-
     // Grabs case_id of the just inserted case and uses it to set aio_id to current aio
     $userId =(int)$_SESSION['userId'];
 
@@ -252,12 +250,12 @@ if(isset($_POST['SaveFormA']) || isset($_POST['SubmitFormA'])){
     
     //header('location: ../AIO/ActiveCases.php');
 
-    // Grabs case_id of the just inserted case and uses it to set aio id to null! 
+    // Grabs case_id of the just inserted case and uses it to set aio id to null
     $userId =(int)$_SESSION['userId'];
 
     $CurrCaseId=(int)$_POST['CurrCaseId'];
     
-    $AcceptCase = $conn->prepare("UPDATE active_cases SET aio_id = NULL  WHERE case_id = ?");
+    $AcceptCase = $conn->prepare("UPDATE active_cases SET aio_id = NULL WHERE case_id = ?");
     $AcceptCase->bind_param("d",$CurrCaseId); //bind evidence folder name to the prepared statements
     if (!$AcceptCase->execute()) {
        echo "Execute failed: (" . $statement->errno . ") " . $statement->error;
