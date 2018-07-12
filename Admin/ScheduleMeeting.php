dd164ce7a2862e1e3c8022c940fbbb870a033c06
Ryan Stevens <ry366081@dal.ca>
2018-07-12 02:09:44 -0300

    More data and more table rows to display that data.

--- Admin/ScheduleMeeting.php
+++ Admin/ScheduleMeeting.php
@@ -47,60 +47,86 @@ include '../includes/formProcess.php';
 										A.description,
 										A.prof_id,
 										A.date_aware,
-										A.evidence_fileDir
+										A.evidence_fileDir,
+										P.fname,
+										P.lname,
+										P.email,
+										P.alt_email
 									FROM
 										active_cases as A
+									INNER JOIN
+										professor as P ON A.prof_id = P.professor_id
 									WHERE
-										case_id = 2
+										case_id = $caseId
 									");
 
 			if(!$statement->execute()){
                 echo "Execute failed: (" . $query->errno . ") " . $query->error;
             }
 
-			$statement->bind_result($caseID, $description, $prof_id, $date_aware, $evidence_path);
+			$statement->bind_result($caseID, $description, $prof_id, $date_aware, $evidence_path, $prof_fname, $prof_lname, $prof_email_1, $prof_email_2);
 
 			$statement->fetch();	//Pull just one row.
 
-			print("\nHello World" . $caseID . "\n" . $description . "\n" . $prof_id . "\n" . $date_aware . "\n" . $evidence_path);
-		?>	
+			if (!$description){
+				$description = "N/A";
+			}
 
-        <div>   
-            <table class="table table-bordered" style="font-size: 14px;">
-                <tbody>
-                    <tr>
-                        <td>Banner number</td>
-                        <td>B00000000</td>
-                    </tr>
-                    <tr>
-                        <td>Student(s) name</td>
-                        <td>Mark Auto</td>
-                    </tr>
-                    <tr>
-                        <td>Professor</td>
-                        <td>Fred</td>
-                    </tr>
-                    <tr>
-                        <td>Date</td>
-                        <td>Jan 31st 2017</td>
-                    </tr>
-                    <tr>
-                        <td>Files</td>
-                        <td><a href="#">Link.zip</a></td>
-                    </tr>            
-                    
-                    <tr>
-                        <td>Case status</td>
-                        <td>Waiting for student to confirm meeting date</td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        </div>
+			//Get Student information
+			/*
+			$statement = $conn->prepare("
+									SELECT
+										A.case_id
+									FROM
+										active_cases as A
+									WHERE
+										case_id = $caseId
+									");
 
-        <div>
-            <!-- Displays the current AIO, or that there isn't one. -->
+			if(!$statement->execute()){
+                echo "Execute failed: (" . $query->errno . ") " . $query->error;
+            }
 
-        </div>
+			$statement->bind_result($caseID);
+
+			$statement->fetch();	//TODO: Loop to pull the rows of all relevant students.
+			*/
+
+			//Make the table.
+			echo <<<ViewCaseInfo
+				<table class="table table-bordered" style="font-size: 14px;">
+					<tbody>
+						<tr>
+							<td>Case description</td>
+							<td>$description</td>
+						</tr>
+						<tr>
+							<td>Student name</td>
+							<td class="studentName">Case ID: $caseID</td>
+						</tr>
+						<tr>
+							<td>Professor</td>
+							<td>$prof_fname $prof_lname</td>
+						</tr>
+							<td>Professor email 1</td>
+							<td>$prof_email_1</td>
+						</tr>
+							<td>Professor email 2</td>
+							<td>$prof_email_2</td>
+						</tr>
+						<tr>
+							<td>Date</td>
+							<td class="date">$date_aware</td>
+						</tr>
+						<tr>
+							<td>Files</td>
+							<!-- What will we want to show here? -->
+							<td><a href="$evidence_path">$evidence_path</a></td>
+						</tr>
+					</tbody>
+				</table>
+ViewCaseInfo;
+		?>
+
     </body>
 </html>
