76db888ce92b0620b898227ebf6f59b4119853b4
Ryan Stevens <ry366081@dal.ca>
2018-07-23 14:23:06 -0300

    Create new proccessFormC file; Hello world

---
+++ Admin/processFormC.php
@@ -0,0 +1,9 @@
+<?php	
+	include_once '../includes/fileFunctions.php';
+	require_once '../includes/session.php';
+
+	$baseEvidenceDir = "../evidence/";
+	$processSuccessful = true;
+	
+	echo "Hello World 2";
+?>
