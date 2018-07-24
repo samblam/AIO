20fd6d7f29faf65bba32c3aa7c2f94afb0378465
blackmamba011 <yadiogbudibe@gmail.com>
2018-07-24 16:45:22 -0300

    Fixed duplicate code from git pull

--- common/CaseInformation.php
+++ common/CaseInformation.php
@@ -80,7 +80,7 @@ if(isset($_POST['caseId'])){
 
                 //Get all relevent feilds and bind them to php variables
                 $conn = OpenCon();
-                $caseIdValue = $_POST['caseId'];
+                //$caseIdValue = $_POST['caseId'];
 
                 $statement = $conn->prepare("
                 SELECT
@@ -193,9 +193,7 @@ if(isset($_POST['caseId'])){
                 </tbody>
             </table>
         </div>
-
-DisplayInfo;
-                ?>
+                
 
                 <div class="center-block text-center">
         <?php 
@@ -272,7 +270,7 @@ AcceptDenyButtons;
                     }
                 
                     $statement->bind_result($caseVerdict);
-                    
+                    while($statement->fetch()){
 
                     if($caseVerdict == NULL){
                         // Insufficient Evidence Button
@@ -281,22 +279,13 @@ AcceptDenyButtons;
                             <form class="delete_this_case" method="post" action="AioActiveCases.php" onclick="return confirm('Are you sure you want to remove this case for insufficient evidence? This will permanently delete the case.\\nClick OK to continue.')">
                                 <input type="text" name="case_id" value="$caseIdValue" hidden>
                                 <button class="btn btn-danger" value="true" type="submit" name="insufficientEvidence">Insufficient Evidence</button>
-                            </form>
-
-                    while($statement->fetch()){
-                        
-                        if($caseVerdict == NULL){
-                            // Insufficient Evidence Button
-                            echo <<<ViewAllPost
-                                <form class="delete_this_case" method="post" action="ActiveCases.php" onclick="return confirm('Are you sure you want to remove this case for insufficient evidence? This will permanently delete the case.\\nClick OK to continue.')">
-                                    <input type="text" name="case_id" value="$caseId" hidden>
-                                    <button class="btn btn-danger" value="true" type="submit" name="insufficientEvidence">Insufficient Evidence</button>
-                                </form>
+                            </form>                 
 ViewAllPost;
                         }
                         else if ($caseVerdict == "guilty"){
                             // Close case Button guilty
                             echo <<<ViewAllPost2
+
                                 <form class="delete_this_case" method="post" action="ActiveCases.php" onclick="return confirm('Are you sure you want to close this case? \\nIf the verdict is guilty the case gets archived in our system, and if the verdict is not guilty the case is permanently deleted. \\nClick OK to continue.')">
                                     <input type="text" name="case_id"   hidden>
                                     <button class="btn btn-danger" value="true" type="submit" name="closeCaseGuilty">Close Case</button>
@@ -311,14 +300,6 @@ ViewAllPost2;
                                 <input type="text" name="case_id" value="$caseIdValue" hidden>
                                 <button class="btn btn-danger" value="true" type="submit" name="closeCaseNotGuilty">Close Case</button>
                             </form>
-                        }
-                        else if ($caseVerdict == "not guilty"){
-                            // Close case Button not guilty
-                            echo <<<ViewAllPost3
-                                <form class="delete_this_case" method="post" action="ActiveCases.php" onclick="return confirm('Are you sure you want to close this case? \\nIf the verdict is guilty the case gets archived in our system, and if the verdict is not guilty the case is permanently deleted. \\nClick OK to continue.')">
-                                    <input type="text" name="case_id" value="$caseId" hidden>
-                                    <button class="btn btn-danger" value="true" type="submit" name="closeCaseNotGuilty">Close Case</button>
-                                </form>
 ViewAllPost3;
                         }
                     }
