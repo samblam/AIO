33668e6506856b1a5cc12a3b9bfe28bead3256bc
blackmamba011 <yadiogbudibe@gmail.com>
2018-07-27 22:11:10 -0300

    Faq Page with dummy text

--- includes/top-header-full.php
+++ includes/top-header-full.php
@@ -1,1 +1,3 @@
 <button class="btn btn-default pull-right" onclick="location.href='../index.php';" type="button">Logout</button>
+<button class="btn btn-default pull-right" onclick="location.href='../faq.php';" type="button">FAQ</button>
+
